c3b28ff0cb07be8e34b2abd3c1e5befeacd7d0d1
Johnx199x <Jonathan.cruz0t@gmail.com>
2025-02-20 10:36:42 -0500

    addVercelSpeedInsights

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { HeaderApp } from './Components/Header/HeaderApp';
 import { FooterApp } from './Components/Footer/FooterApp';
 import { MainContent} from "./Components/Body/MainContent" 
 import { SideBar } from "./Components/SideBar/SideBar";
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function App() {
   const [zone, setZone] = useState("Monsters")
@@ -20,6 +20,7 @@ function App() {
       <SideBar ubi={changeZone} />
       <MainContent content={zone} />
       <FooterApp />
+      <SpeedInsights />
     </div>
   );
 }
