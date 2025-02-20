674748a5cb9d773cdf01a18a11afcdc45bcba0f4
Johnx199x <Jonathan.cruz0t@gmail.com>
2025-02-20 10:18:28 -0500

    fixReadMe

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { HeaderApp } from './Components/Header/HeaderApp';
 import { FooterApp } from './Components/Footer/FooterApp';
 import { MainContent} from "./Components/Body/MainContent" 
 import { SideBar } from "./Components/SideBar/SideBar";
+
+
 function App() {
   const [zone, setZone] = useState("Monsters")
 
