75d1492e5b1b2051231b27833032f0eeeb7b21b0
Johnx199x <Jonathan.cruz0t@gmail.com>
2025-02-21 15:00:35 -0500

    addIconMonsterSpecialAttack

--- src/Components/Body/Monsters/Monsters.js
+++ src/Components/Body/Monsters/Monsters.js
@@ -109,6 +109,42 @@ export const Monsters = () => {
                 </>
             )
         }
+        const monsterSpecialAttack =(arrEle)=>{
+            let elements=[];
+            arrEle.forEach((e)=>{
+                
+                switch(e){
+                    case "Fireblight": elements.push(<img src={require("../../../Assets/icons/StatusEffect/Status_Effect-Severe_Fireblight_Icon.webp")}  alt='fireBlight_status_img' title='Loose life at time'></img>); break;
+                    case "Thunderblight": elements.push(<img src={require("../../../Assets/icons/StatusEffect/Status_Effect-Thunderblight_Icon.webp")} alt='thunderBlight_img' title='More stun chance'></img>);break;
+                    case "Waterblight": elements.push(<img src={require("../../../Assets/icons/StatusEffect/Status_Effect-Severe_Waterblight_Icon.webp")} alt='waterblight_img' title='Loose stamina'></img>);break;
+                    case "Iceblight": elements.push(<img src={require("../../../Assets/icons/StatusEffect/Status_Effect-Iceblight_Icon.webp")} alt='iceBlight_img'></img>);break;
+                    case "Dragonblight": elements.push(<img src={require("../../../Assets/icons/StatusEffect/Status_Effect-Dragonblight_Icon.webp")} alt='dracoblight_img'></img>);break;
+                    case "Poison": elements.push(<img src={require("../../../Assets/icons/StatusEffect/Status_Effect-Deadly_Poison.webp")} alt='poison-status_img' title='Poison effect'></img>);break;
+                    case "Snowman": elements.push(<img src={require("../../../Assets/icons/StatusEffect/StatusEffect-SnowMan.webp")} alt='Snowman-status_img' title='SnowMan effect'></img>);break;
+                    case "Soiled": elements.push(<img src={require("../../../Assets/icons/StatusEffect/StatusEffect-Soiled.webp")} alt='Soiled-status_img' title='Consumables are not allowed.'></img>);
+                    break;
+                    case "Stun": elements.push(<img src={require("../../../Assets/icons/StatusEffect/Status-Stun.webp")} alt='Stun-status_img' title='Stun-Status'></img>);break;
+                    case "Paralysis": elements.push(<img src={require("../../../Assets/icons/StatusEffect/Status-Paralysis.webp")} alt='Paralysis-status_img' title='Paralysis-Status'></img>);break;
+                    case "Life drain": elements.push(<img src={require("../../../Assets/icons/StatusEffect/StatusEffect-Leeched.webp")} alt="Life-Drain-status_img" title='Life Drain'></img>);break;
+                    case "Sleep": elements.push(<img src={require("../../../Assets/icons/StatusEffect/Status-Sleep.webp")} alt='Slee[-status_img' title='Slepp-Status'></img>);break;
+                    case "Defence Down": elements.push(<img src={require("../../../Assets/icons/StatusEffect/StatusEffect-DefenseDow.webp")} alt='Defense-Down-status_img' title='Loose defence'></img>);break;
+                    case "Muddy": elements.push(<img src={require("../../../Assets/icons/StatusEffect/Status_Effect-Webbed_MH4_Icon.webp")} alt='Webbed-status_img' title=''></img>);break;
+
+
+
+
+
+
+                    default: elements.push(e);
+            }
+            })
+            return(
+                <>
+                    {elements.map((e,index)=> <span key={index}
+                    className='span-element-icon'>{e}</span>  )}
+                </>
+            )
+        }
         
         
         return(
@@ -117,7 +153,7 @@ export const Monsters = () => {
                 <p><span className='monster-info-tags'>Monster type: </span>{props.type}</p>
                 <p><span className='monster-info-tags'>Danger: </span>{monsterDanger()}</p>
                 <p><span className='monster-info-tags'>Monster element: </span>{props.element===undefined ? "None" : monsterElement(props.element)}</p>
-                <p><span className="monster-info-tags">Special Attack Status:</span>{props.ailments=== undefined?"None ": props.ailments.join(", ")}</p>
+                <p><span className="monster-info-tags">Special Attack Status:</span>{props.ailments=== undefined?"None ": monsterSpecialAttack(props.ailments)}</p>
                 <p><span className='monster-info-tags'>Monster weakness: </span>{props.weakness===undefined?"None weakness discovered":monsterElement(props.weakness)}</p>
             </div>
         )
