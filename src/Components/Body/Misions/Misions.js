6354704a789b4b06d85d0c748c0ee376b1f998ef
Johnx199x <Jonathan.cruz0t@gmail.com>
2025-02-26 17:47:18 -0500

    Add 1★ village Quest

--- src/Components/Body/Misions/Misions.js
+++ src/Components/Body/Misions/Misions.js
@@ -1,6 +1,25 @@
 import React from 'react'
 
 export const Misions = () => {
+
+  /*{
+            "_id": {
+                "$oid": "" 
+            },
+            "name": "",
+            "client": "", 
+            "description": "", 
+            "reward":  ,
+            "map": "", 
+            "isKey": true, 
+            "questType": "Village", 
+            "game": "Monster Hunter Portable 3rd", 
+            "difficulty": "", 
+            "objective": "", 
+            "targets": [
+                ""
+            ]
+        }, */
   return (
     <h3>Under contruction...</h3>
   )
